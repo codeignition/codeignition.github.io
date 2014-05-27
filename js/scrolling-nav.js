486a622c8fb0f4c58d3bc0221e31832dfbc878ff
nida-fatma <nida.biet@gmail.com>
2014-05-27 10:35:42 +0530

    [nida]-scrolling added

---
+++ js/scrolling-nav.js
@@ -0,0 +1,10 @@
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.topmenuitem a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
