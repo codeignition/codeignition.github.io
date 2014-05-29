b43677f5125ef5dd015f7a90116ac1437a1b2881
nida-fatma <nida.biet@gmail.com>
2014-05-29 08:38:27 +0530

    [nida]-fixing nav

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -3,7 +3,7 @@ $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top-25
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
