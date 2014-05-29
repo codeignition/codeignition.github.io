2a71eaaa69726f9db8ae0c7319dc82c48c637602
nida-fatma <nida.biet@gmail.com>
2014-05-29 18:29:26 +0530

    [nida]-fade-in text

--- _site/js/scrolling-nav.js
+++ _site/js/scrolling-nav.js
@@ -1,10 +1,26 @@
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top-25
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('.page-scroll a').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top-25
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 });
+
+setInterval(function() {
+  $('.main_unit_text').fadeOut(500, function() {
+    var $this = $(this);
+    if ($this.text() == 'Handcrafted infrastructure for beautifully written apps.') {
+                     $this.text('Great products deserve smart infrastructure.');
+    }
+    else if ($this.text() == 'Great products deserve smart infrastructure.') {
+                 $this.text('Awesome ideas needs beautifully written applications.');
+    }
+    else      {
+       $this.text('Handcrafted infrastructure for beautifully written apps.');
+    }
+    $this.fadeIn(500);
+  });
+}, 5000);
