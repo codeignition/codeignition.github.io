0e8c191ede998647df865edabd9f4476c97f36a5
nida-fatma <nida.biet@gmail.com>
2014-06-03 13:20:17 +0530

    [nida]-fixing js

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,7 +1,7 @@
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
   $('.page-scroll a').bind('click', function(event) {
-    var $anchor = $(this);
+    var $anchor = "{{ site.baseurl }}"/(this);
     $('html, body').stop().animate({
       scrollTop: $($anchor.attr('href')).offset().top-25
     }, 1500, 'easeInOutExpo');
